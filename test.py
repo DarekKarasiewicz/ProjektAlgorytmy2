9c960fa0c167a0cccc5b774abb1ef58e730e32b0
Darek <dar.karasiewicz@gmail.com>
2022-01-21 19:27:25 +0100

    huffman test

--- test.py
+++ test.py
@@ -1,11 +1,7 @@
 import unittest
 import darek_huffman
 
-class Simple_test(unittest.TestCase):
-    cleartext = "daa"
-    ciphertext = "111010"
-    code = { "d" : "11" , "a" : "10" }
-
+class Huffman_test():
     def test_encode(self):
         ct, cd = darek_huffman.encode.encode(self.cleartext)
         self.assertEqual(self.ciphertext,ct) 
@@ -14,19 +10,17 @@ class Simple_test(unittest.TestCase):
     def test_decode(self): 
         cl = darek_huffman.decode.decode(self.ciphertext,self.code)
         self.assertEqual(self.cleartext,cl) 
+
+class Simple_test(Huffman_test,unittest.TestCase):
+    cleartext = "daa"
+    ciphertext = "111010"
+    code = { "d" : "11" , "a" : "10" }
+
     
-class Compicated_test(unittest.TestCase):
+class Compicated_test(Huffman_test,unittest.TestCase):
     cleartext = "daareeekkkkk"
     ciphertext = "1111111101110111101101101101010101010"
     code = {'d': '11111', 'a': '1110', 'r': '11110', 'e': '110', 'k': '10'}
 
-    def test_encode(self):
-        ct, cd = darek_huffman.encode.encode(self.cleartext)
-        self.assertEqual(self.ciphertext,ct) 
-        self.assertEqual(self.code,cd)
-
-    def test_decode(self): 
-        cl = darek_huffman.decode.decode(self.ciphertext,self.code)
-        self.assertEqual(self.cleartext,cl) 
 
 unittest.main()
